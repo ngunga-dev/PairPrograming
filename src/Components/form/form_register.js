2355e3d3965032af56d7b87ab227d3f1780763c4
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-08 01:25:52 -0300

    fixing fields

--- src/Components/form/form_register.js
+++ src/Components/form/form_register.js
@@ -3,20 +3,28 @@ function FormRegister () {
         <div className="form_box">
           <div className="form_register">
             <form action="" method="">
-              <h2>Register Product</h2>
+              <h2>Register New Product</h2>
               <div className="form-items">
                 <input type="text" name="product_name" placeholder="Product Name" required/>
+                <input type="text" name="product_author" placeholder="Product Author" required/>
                 <input type="file" name="product_image" placeholder="Product Image" required/>             
                 <input type="text" name="product_description" placeholder="Product Description" required/>
                 <select name="select">
-                    <option value="valor1">Category 01</option>
-                    <option value="valor2" selected>Category 02</option>
-                    <option value="valor3">Category 03</option>
+                    <option value="null" selected>- select a category -</option>
+                    <option value="category1">Category01</option>
+                    <option value="category2">Category02</option>
+                    <option value="category3">Category03</option>
                 </select>
-                <input type="text" name="product_price" placeholder="Product Price" required/>             
+                <select name="select">
+                    <option value="null" selected>- digital or paper -</option>
+                    <option value="category1">digital book</option>
+                    <option value="category2">paper book</option>
+                </select>
+
+                <input type="number" name="product_price" placeholder="Product Price (R$)" required min="0.00" max="10000.00" step="0.01"/>             
                 <input type="number" name="product_quantity" placeholder="Product Quantity" required/>             
                
-                <input type="submit" value="Register"/> {/* Hide this button after register a product */}
+                <input type="submit" value="Add Product"/> {/* Hide this button after register a product */}
                 <input type="submit" value="Edit"/>  {/* Show this button after register a product */}
                 <input type="submit" value="Delete"/> {/* Show this button after register a product */}
               </div>
