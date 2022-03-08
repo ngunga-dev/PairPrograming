464c11a6cdc0b73d7298d85a3026326eef14da07
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-07 22:20:16 -0300

    add comments about show/hide buttons

--- src/Components/form/form_register.js
+++ src/Components/form/form_register.js
@@ -1,21 +1,24 @@
 function FormRegister () {
     return (
-        <div className="contact">
-          <div className="form">
+        <div className="form_box">
+          <div className="form_register">
             <form action="" method="">
               <h2>Register Product</h2>
-              <div className="items-form">
+              <div className="form-Items">
                 <input type="text" name="product_name" placeholder="Product Name" required/>
+                <input type="file" name="product_image" placeholder="Product Image" required/>             
                 <input type="text" name="product_description" placeholder="Product Description" required/>
-                <input type="text" name="product_description" placeholder="Product Description" required/>             
                 <select name="select">
-                    <option value="valor1">Valor 1</option>
-                    <option value="valor2" selected>Valor 2</option>
-                    <option value="valor3">Valor 3</option>
+                    <option value="valor1">Category 01</option>
+                    <option value="valor2" selected>Category 02</option>
+                    <option value="valor3">Category 03</option>
                 </select>
-                <input type="submit" value="Register"/>
-                <input type="submit" value="Edit"/>
-                <input type="submit" value="Delete"/>
+                <input type="text" name="product_price" placeholder="Product Price" required/>             
+                <input type="number" name="product_quantity" placeholder="Product Quantity" required/>             
+               
+                <input type="submit" value="Register"/> {/* Hide this button after register a product */}
+                <input type="submit" value="Edit"/>  {/* Show this button after register a product */}
+                <input type="submit" value="Delete"/> {/* Show this button after register a product */}
               </div>
             </form>
           </div>
